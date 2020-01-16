7d0a14c2fd9f6238940bcc2bd11867c5d6f55b0e
Pranav Pandhare <pranav.pandhare@desiretec.com>
2020-01-16 13:15:45 +0100

    api connection

---
+++ config/module.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    'groups' => [
+        'table' => 'groups',
+    ],
+    'pages' => [
+        'table' => 'pages',
+    ],
+    'email_templates'=> [
+        'table'              => 'email_templates',
+        'placeholders_table' => 'email_template_placeholders',
+        'types_table'        => 'email_template_types',
+    ],
+    'blog_tags' => [
+        'table' => 'blog_tags',
+    ],
+    'blog_categories' => [
+        'table' => 'blog_categories',
+    ],
+    'blogs' => [
+        'table' => 'blogs',
+    ],
+    'wishes' => [
+        'table' => 'wishes',
+    ],
+    'offers' => [
+        'table' => 'offers',
+    ],
+    'offer_files' => [
+        'table' => 'offer_files',
+    ],
+    'whitelabels' => [
+        'table' => 'whitelabels',
+    ],
+    'agents' => [
+        'table' => 'agents',
+    ],
+    'distributions' => [
+        'table' => 'distributions',
+    ],
+    'comments' => [
+        'table' => 'comments',
+    ],
+    'contact' => [
+        'table' => 'contacts',
+    ],
+    'faqs' => [
+        'table' => 'faqs',
+    ],
+    'attachments' => [
+        'table' => 'attachments',
+    ],
+];
