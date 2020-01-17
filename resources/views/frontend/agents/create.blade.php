f2c8aea5a5aca6b316eb8561d9951fee217ffaf0
Milena Petkanova <milena.petkanova@motion-software.com>
2020-01-17 15:52:10 +0200

    update agent views

--- resources/views/frontend/agents/create.blade.php
+++ resources/views/frontend/agents/create.blade.php
@@ -21,27 +21,5 @@
             {{ link_to_route('frontend.agents.index', 'Cancel', [], ['class' => 'btn btn-danger btn-md']) }}
             {{ Form::submit('Create', ['class' => 'btn btn-primary btn-md']) }}
         </div>
-    {{ Form::close() }}   
-    
-@endsection
-
-@section("after-scripts")
-    <script type="text/javascript">
-        $(document).on('change','.up', function(){
-            var names = [];
-            var length = $(this).get(0).files.length;
-            for (var i = 0; i < $(this).get(0).files.length; ++i) {
-                names.push($(this).get(0).files[i].name);
-            }
-            // $("input[name=file]").val(names);
-            if(length>2){
-                var fileName = names.join(', ');
-                $(this).closest('.form-group').find('.form-control').attr("value",length+" files selected");
-            }
-            else{
-                $(this).closest('.form-group').find('.form-control').attr("value",names);
-            }
-        });
-
-    </script>
+    {{ Form::close() }}
 @endsection
