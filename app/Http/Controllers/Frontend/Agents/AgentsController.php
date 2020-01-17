d991e9a5a0de9d2629d7b21a867021533a1468a9
Milena Petkanova <milena.petkanova@motion-software.com>
2020-01-17 15:51:18 +0200

    use api service in controller

--- app/Http/Controllers/Frontend/Agents/AgentsController.php
+++ app/Http/Controllers/Frontend/Agents/AgentsController.php
@@ -2,165 +2,89 @@
 
 namespace App\Http\Controllers\Frontend\Agents;
 
-use App\Models\Agents\Agent;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use GuzzleHttp\Client;
+use App\Services\Api\ApiService;
+use App\Models\Agents\Agent;
 
 /**
- * Class AgentsController.
+ * Class AgentsController
  */
 class AgentsController extends Controller
 {
     const BODY_CLASS = 'agent';
 
-    protected $status = [
-        'Active'       => 'Active',
-        'Inactive'     => 'Inactive',
-        'Deleted'      => 'Deleted',
-    ];
+    protected $agent;
+    protected $apiService;
 
-    public function __construct()
+    public function __construct(ApiService $apiService, Agent $agent)
     {
+        $this->apiService = $apiService;
+        $this->agent = $agent;
     }
 
     public function index()
     {
-        $client = new Client();
-        $response = $client->get('http://localhost:8000/api/v1/agents',
-            [
-                'headers' => [
-                    'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDAvYXBpL3YxL2F1dGgvbG9naW4iLCJpYXQiOjE1NzkwMTc2MDYsImV4cCI6MTU3OTEwNDAwNiwibmJmIjoxNTc5MDE3NjA2LCJqdGkiOiJpWmJFNXdQR2lVQlB0OFFHIiwic3ViIjoxLCJwcnYiOiI5NGRiZDk2MWFhZWYwZTNjZTY2YWQ3ZDUwZTY0NzcxNzYwOWRkYTI0IiwiaWQiOjF9.USxiUfBgA_oAhm5rngyx2bXflp9qTq1pnkAdXkkFsWM'
-                ]
-            ]
-        );
-        $response_json = json_decode($response->getBody());
-        $agents = $response_json->data;
+        $response = $this->apiService->getAll('/agents');
+
+        $this->apiService->validate($response->getStatusCode());
+
+        $agents = json_decode($response->getBody())->data;
 
         return view('frontend.agents.index')->with([
-            'status'     => $this->status,
             'body_class' => $this::BODY_CLASS,
-            'agents'     => $agents
+            'agents'     => $agents,
         ]);
     }
 
-    /**
-     * @param \App\Models\Agents\Agent                               $agent
-     * @param \App\Http\Requests\Frontend\Agents\ManageAgentsRequest $request
-     *
-     * @return mixed
-     */
     public function profile()
     {
         return view('frontend.agents.profile');
     }
 
-    /**
-     * @param \App\Http\Requests\Frontend\Agents\ManageAgentsRequest $request
-     * @param type                                                   $id
-     *
-     * @return mixed
-     */
     public function create()
     {
         return view('frontend.agents.create')->with([
-            'status'         => $this->status,
-            'body_class'     => $this::BODY_CLASS,
+            'body_class'  => $this::BODY_CLASS,
+            'agent'       => $this->agent,
         ]);
     }
 
-    /**
-     * @param \App\Http\Requests\Frontend\Agents\StoreAgentsRequest $request
-     *
-     * @return mixed
-     */
-    public function store()
+    public function store(Request $request, Agent $agent)
     {
-        $this->agent->create($request->except('_token'));
+        $response = $this->apiService->create('/agents/create', $agent);
+
+        $this->apiService->validate($response->getStatusCode());
 
         return redirect()
             ->route('frontend.agents.index')
             ->with('flash_success', trans('alerts.frontend.agents.created'));
     }
 
-    /**
-     * @param \App\Models\Agents\Agent                               $agent
-     * @param \App\Http\Requests\Frontend\Agents\ManageAgentsRequest $request
-     *
-     * @return mixed
-     */
     public function edit()
     {
         return view('frontend.agents.edit')->with([
-            // 'agent'               => $agent,
-            'status'              => $this->status,
-            'body_class'          => $this::BODY_CLASS,
+            'body_class'  => $this::BODY_CLASS,
+            'agent'       => $this->agent,
         ]);
     }
 
-    public function editAgent()
+    public function update(Request $request, Agent $agent)
     {
-        // $agent = DB::table('agents')->where('id', $id)->first();
+        $response = $this->apiService->update('/agents/update/' . $agent->id, $agent);
 
-        return view('frontend.agents.edit')->with([
-            // 'agent'               => $agent,
-            'status'              => $this->status,
-            'body_class'          => 'agent',
-        ]);
-    }
-
-    /**
-     * @param \App\Models\Agents\Agent                               $agent
-     * @param \App\Http\Requests\Frontend\Agents\UpdateAgentsRequest $request
-     *
-     * @return mixed
-     */
-    public function update()
-    {
-        // $input = $request->all();
-
-        // $this->agent->update($agent, $request->except(['_token', '_method']));
+        $this->apiService->validate($response->getStatusCode());
 
         return redirect()
             ->route('admin.agents.index')
             ->with('flash_success', trans('alerts.frontend.agents.updated'));
     }
 
-    public function updateAgent()
+    public function delete($id)
     {
-        // $this->agent->doUpdate($id, $request);
+        $this->apiService->delete('/agents/delete/' . $id);
 
-        return redirect()
-            ->route('frontend.agents.index')
-            ->with('flash_success', trans('alerts.frontend.agents.updated'));
-    }
-
-    /**
-     * @param \App\Models\Agents\Agent                               $agent
-     * @param \App\Http\Requests\Frontend\Agents\ManageAgentsRequest $request
-     *
-     * @return mixed
-     */
-    public function destroy()
-    {
-        // $this->agent->delete($agent);
-
-        return redirect()
-            ->route('admin.agents.index')
-            ->with('flash_success', trans('alerts.frontend.agents.deleted'));
-    }
-
-    public function status()
-    {
-        // $this->agent->updateStatus($id);
-        // $this->session->put('agent_id', $id);
-
-        return redirect()->back();
-    }
-
-    public function delete()
-    {
-        /**** Delete Agent and Assign offers/messages to another agent ****/
-        // $this->agent->deleteAgent($id);
+        $this->apiService->validate($response->getStatusCode());
 
         return redirect()
             ->route('frontend.agents.index')
