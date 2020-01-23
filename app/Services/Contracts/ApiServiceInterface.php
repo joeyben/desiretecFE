d4fd73a2a23d9f57cccdb0bcdee8abfaefac5a7c
Milena Petkanova <milena.petkanova@motion-software.com>
2020-01-24 01:42:45 +0200

    update ApiService

--- app/Services/Contracts/ApiServiceInterface.php
+++ app/Services/Contracts/ApiServiceInterface.php
@@ -4,11 +4,17 @@ namespace App\Services\Contracts;
 
 Interface ApiServiceInterface
 {
-    public function get(string $endpoint);
+    public function setAuthorization(?string $token);
+
+    public function setHeader(string $key, string $value);
+
+    public function get(string $endpoint, array $data);
 
     public function post(string $endpoint, array $data);
 
     public function put(string $endpoint, array $data);
 
     public function delete(string $endpoint);
+
+    public function formatResponse(string $format);
 }
