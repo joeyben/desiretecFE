e91dc90382930bc1e4f5229944b3ca9d3e6e3923
Joe <joe@thisisleap.com>
2020-02-04 04:57:02 +0100

    WL call

--- routes/Frontend/Frontend.php
+++ routes/Frontend/Frontend.php
@@ -31,32 +31,32 @@ Route::domain('{subdomain}.wish-service.com')->group(function () {
     * These frontend controllers require the user to be logged in
     * All route names are prefixed with 'frontend.'
     */
-    Route::group(['middleware' => 'auth'], function () {
+    Route::group(['middleware' => 'auth'], function ($subdomain) {
 
-        Route::group(['namespace' => 'Users', 'as' => 'user.'], function () {
+        Route::group(['namespace' => 'Users', 'as' => 'user.'], function ($subdomain) {
             Route::get('account', 'AccountController@index')->name('account');
             Route::put('account/update/{id}', 'AccountController@update')->name('update');
             Route::patch('account/profilepic/update', 'AccountController@updateProfilePicture')->name('profile-picture');
         });
 
-        Route::group(['namespace' => 'Comments', 'as' => 'comments.'], function () {
+        Route::group(['namespace' => 'Comments', 'as' => 'comments.'], function ($subdomain) {
             Route::get('comments', 'CommentsController@index')->name('index');
             Route::post('comment/store', 'CommentsController@store')->name('store');
         });
 
-        Route::group(['namespace' => 'Contact', 'as' => 'contact.'], function () {
+        Route::group(['namespace' => 'Contact', 'as' => 'contact.'], function ($subdomain) {
             Route::post('contact/store', 'ContactController@store')->name('store');
             Route::post('callback/store', 'ContactController@storeCallback')->name('storecallback');
         });
 
-        Route::group(['namespace' => 'Messages', 'as' => 'messages.'], function () {
+        Route::group(['namespace' => 'Messages', 'as' => 'messages.'], function ($subdomain) {
             Route::get('messages/{wish}/{group}', 'MessagesController@list')->name('list');
             Route::post('messages', 'MessagesController@create')->name('create');
             Route::post('messages/{id}', 'MessagesController@update')->name('update');
             Route::get('messages/{id}', 'MessagesController@delete')->name('delete');
         });
 
-        Route::group(['namespace' => 'Offers', 'as' => 'offers.'], function () {
+        Route::group(['namespace' => 'Offers', 'as' => 'offers.'], function ($subdomain) {
             Route::get('offers', 'OffersController@index')->name('index');
             Route::post('offers/get', 'OffersTableController')->name('get');
             Route::get('offers/create/{id}', 'OffersController@create')->name('create');
@@ -69,7 +69,7 @@ Route::domain('{subdomain}.wish-service.com')->group(function () {
             Route::post('wish/getoffers', 'OffersTableController@showOffersForWish')->name('wishoffers');
         });
 
-        Route::group(['namespace' => 'Agents', 'as' => 'agents.'], function () {
+        Route::group(['namespace' => 'Agents', 'as' => 'agents.'], function ($subdomain) {
             Route::get('agents', 'AgentsController@index')->name('index');
             Route::get('agent/profile', 'AgentsController@profile')->name('profile');
             Route::get('agents/create', 'AgentsController@create')->name('create');
@@ -79,7 +79,7 @@ Route::domain('{subdomain}.wish-service.com')->group(function () {
             Route::get('agents/delete/{id}', 'AgentsController@delete')->name('delete');
         });
 
-        Route::group(['namespace' => 'Wishes', 'as' => 'wishes.'], function () {
+        Route::group(['namespace' => 'Wishes', 'as' => 'wishes.'], function ($subdomain) {
             Route::get('wishlist', 'WishesController@wishList')->name('list');
             Route::get('wishes/getlist', 'WishesController@getList')->name('getlist');
 
