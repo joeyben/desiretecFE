fa5dc50318459be9538e026a16f9a8ea65090edc
Joe <joe@desiretec.com>
2020-02-18 10:55:00 +0000

    wish creation

---
+++ resources/views/frontend/whitelabel/created.blade.php
@@ -0,0 +1,9 @@
+<div class="kwp-content kwp-completed-master">
+    <h1><i class="master-icon--check"></i>Vielen Dank, Ihr Reisewunsch wurde versandt.</h1>
+    <p>
+        Ein Berater aus dem Reisebüro nimmt sich Ihrer Wünsche an.
+    </p>
+</div>
+<script>
+    dt.Tracking.rawEvent('form_success', 'success', 'Form sent successfully');
+</script>
