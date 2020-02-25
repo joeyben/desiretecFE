ad5439f0d8d05ad4c1c7723fff0eec5b28317355
Joe <joe@thisisleap.com>
2020-02-25 16:07:10 +0100

    fix suns

--- resources/views/frontend/autooffer/list_tt.blade.php
+++ resources/views/frontend/autooffer/list_tt.blade.php
@@ -5,8 +5,8 @@
 @endsection
 
 @section("after-styles")
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/jquery.slick/1.5.5/slick.css" />
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/jquery.slick/1.5.5/slick-theme.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js" />
 @endsection
 
 @section('content')
