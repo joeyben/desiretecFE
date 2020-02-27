51c8d9dc82f8aa1146489f21a656b8c81ad0b6c2
Milena Petkanova <milena.petkanova@motion-software.com>
2020-02-27 12:26:27 +0200

    apply float-label on login blades

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -11,83 +11,42 @@
 @endsection
 
 @section('content')
-
-    <div class="row form-group justify-content-md-center">
+    <div class="form-group row justify-content-md-center">
         @include('includes.alert')
         <div class="col-md-6 col-md-offset-2">
-
             <div class="card card-login">
                 <div class="card-header">
-                    <div class="text p-3 pb-0">
-                        {{ trans('labels.frontend.auth.login_box_title') }}
-                    </div>
+                    <h4>{{ trans('labels.frontend.auth.login_box_title') }}</h4>
                 </div>
-
                 <div class="card-body">
-
                     {{ Form::open(['route' => ['frontend.auth.api.login', $subdomain], 'class' => 'form-horizontal']) }}
 
-                    <div class="form-group p-2 mb-0">
-                        {{ Form::label('email', trans('validation.attributes.frontend.register-user.email'), ['class' => 'col-sm-4 col-form-label']) }}
-                        <div class="col-sm-12">
-                            {{ Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.register-user.email')]) }}
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
+                        <div class="form-group float-label">
+                            {{ Form::input('email', 'email', null, ['id' => 'email', 'class' => 'form-control', 'placeholder' => 'invisible', 'required']) }}
+                            {{ Form::label('email', trans('validation.attributes.frontend.register-user.email')) }}
+                        </div>
 
-                    <div class="form-group p-2 mb-0">
-                        {{ Form::label('password', trans('validation.attributes.frontend.register-user.password'), ['class' => 'col-sm-4 col-form-label']) }}
-                        <div class="col-sm-12">
-                            {{ Form::input('password', 'password', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.register-user.password')]) }}
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
+                        <div class="form-group float-label mb-2">
+                            {{ Form::input('password', 'password', null, ['id' => 'password', 'class' => 'form-control', 'placeholder' => 'invisible', 'required']) }}
+                            {{ Form::label('password', trans('validation.attributes.frontend.register-user.password')) }}
+                        </div>
 
-                    <div class="form-group p-2">
-                        <div class="col-md-12 col-md-offset-4">
+                        <div class="form-group">
                             <div class="checkbox">
                                 <label>
                                     {{ Form::checkbox('remember') }} {{ trans('labels.frontend.auth.remember_me') }}
                                 </label>
                             </div>
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
+                        </div>
 
-                    <div class="form-group p-2 mb-2">
-                        <div class="col-md-12 col-md-offset-4">
+                        <div class="form-group">
                             {{ Form::submit(trans('labels.frontend.auth.login_button'), ['class' => 'btn btn-primary', 'style' => 'margin-right:15px']) }}
-                            <a href="{{ route('frontend.auth.password.reset', [$subdomain]) }}">{{ trans('labels.frontend.passwords.forgot_password') }}</a>
-                        </div><!--col-md-6-->
-                    </div><!--form-group-->
+                            <a href="{{ route('frontend.auth.password.reset', [$subdomain]) }}" class="link-btn">{{ trans('labels.frontend.passwords.forgot_password') }}</a>
+                        </div>
 
                     {{ Form::close() }}
-
-                    <div class="row text-center">
-
-                    </div>
-                </div><!-- panel body -->
-
-            </div><!-- panel -->
-
-        </div><!-- col-md-8 -->
-
-    </div><!-- row -->
-
-@endsection
-
-@section('after-scripts')
-    <script type="application/javascript">
-        $(document).ready(function(){
-            $('.btn-primary').css({
-                'background': brandColor,
-                'border': '1px solid ' + brandColor,
-                'color': '#fff',
-            });
-            $("input").focus(function(){
-                $(this).css({'border-color': brandColor});
-            });
-            $("input").blur(function(){
-                $(this).css({'border-color': 'inherit'});
-            });
-            $('.form-group a').css({'color': brandColor});
-        });
-    </script>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/frontend/auth/tokenlogin.blade.php
+++ resources/views/frontend/auth/tokenlogin.blade.php
@@ -12,50 +12,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-md-center">
-        <div class="col-md-8 col-md-offset-2">
+    <div class="form-group row justify-content-md-center">
+        <div class="col-md-6 col-md-offset-2">
             <div class="card card-login">
                 <div class="card-header">
-                    <div class="text p-2">
-                    {{ trans('labels.frontend.auth.login_box_title') }}
-                    </div>
+                    <h4>{{ trans('labels.frontend.auth.login_box_title') }}</h4>
                 </div>
                 <div class="card-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('frontend.auth.api.link', [$subdomain]) }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} mb-0">
-                            <div class="row p-2">
-                                <div class="col-md-4">
-                                    <label for="email" class="control-label">{{ trans('labels.frontend.tokenlogin.email') }}</label>
-                                </div>
-                                <div class="col-md-8">
-                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} float-label">
+                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="invisible" required>
+                            <label for="email" class="control-label">{{ trans('labels.frontend.tokenlogin.email') }}</label>
 
-                                    @if ($errors->has('email'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('email') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
                         <div class="form-group">
-                            <div class="row p-2 pb-0">
-                                <div class="col-md-4">
-                                    &nbsp;
-                                </div>
-
-                                <div class="col-md-8">
-                                    <button type="submit" class="btn primary-btn">
-                                        {{ trans('button.tokenlogin.send') }}
-                                    </button>
-                                    <a href="{{route('frontend.auth.login', [$subdomain])}}" class="btn secondary-btn">
-                                        {{ trans('account.login.seller') }}
-                                    </a>
-                                </div>
-                            </div>
+                            <button type="submit" class="btn primary-btn mb-3">
+                                {{ trans('button.tokenlogin.send') }}
+                            </button>
+                            <a href="{{route('frontend.auth.login', [$subdomain])}}" class="btn secondary-btn mb-3">
+                                {{ trans('account.login.seller') }}
+                            </a>
                         </div>
                     </form>
                 </div>
@@ -63,26 +47,4 @@
         </div>
     </div>
 </div>
-@endsection
-
-@section('after-scripts')
-    <script type="application/javascript">
-        $(document).ready(function(){
-            $('.primary-btn').css({
-                'background': brandColor,
-                'border': '1px solid ' + brandColor,
-                'color': '#fff',
-            });
-            $('.secondary-btn').css({
-                'border': '1px solid ' + brandColor,
-                'color': brandColor,
-            });
-            $("input").focus(function(){
-                $(this).css({'border-color': brandColor});
-            });
-            $("input").blur(function(){
-                $(this).css({'border-color': 'inherit'});
-            });
-        });
-    </script>
 @endsection
