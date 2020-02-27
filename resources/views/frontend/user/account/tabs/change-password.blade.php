147ea41935557b75a2d71ea2283be0cf1e5d5753
Milena Petkanova <milena.petkanova@motion-software.com>
2020-02-27 14:04:41 +0200

    apply the improvement on account blades

--- resources/views/frontend/user/account/tabs/change-password.blade.php
+++ resources/views/frontend/user/account/tabs/change-password.blade.php
@@ -1,27 +1,22 @@
 {{ Form::open(['route' => ['frontend.user.changePassword', $subdomain], 'class' => 'form-horizontal', 'method' => 'put']) }}
 
-    <div class="form-group">
-        <div class="col-md-12">
-            {{ Form::input('password', 'old_password', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.register-user.old_password')]) }}
-        </div>
+    <div class="form-group float-label">
+        {{ Form::input('password', 'old_password', null, ['id' => 'old_password', 'class' => 'form-control', 'placeholder' => 'transparent']) }}
+        {{ Form::label('old_password', trans('validation.attributes.frontend.register-user.old_password')) }}
+    </div>
+
+    <div class="form-group float-label">
+        {{ Form::input('password', 'password', null, ['id' => 'password', 'class' => 'form-control', 'placeholder' => 'transparent']) }}
+        {{ Form::label('password', trans('validation.attributes.frontend.register-user.new_password')) }}
+    </div>
+
+    <div class="form-group float-label">
+        {{ Form::input('password', 'password_confirmation', null, ['id' => 'password_confirmation', 'class' => 'form-control', 'placeholder' => 'transparent']) }}
+        {{ Form::label('password_confirmation', trans('validation.attributes.frontend.register-user.new_password_confirmation')) }}
     </div>
 
     <div class="form-group">
-        <div class="col-md-12">
-            {{ Form::input('password', 'password', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.register-user.new_password')]) }}
-        </div>
-    </div>
-
-    <div class="form-group">
-        <div class="col-md-12">
-            {{ Form::input('password', 'password_confirmation', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.register-user.new_password_confirmation')]) }}
-        </div>
-    </div>
-
-    <div class="form-group">
-        <div class="col-md-6 col-md-offset-4">
-            {{ Form::submit(trans('labels.general.buttons.update'), ['class' => 'btn primary-btn', 'id' => 'change-password']) }}
-        </div>
+        {{ Form::submit(trans('labels.general.buttons.update'), ['class' => 'btn primary-btn', 'id' => 'change-password']) }}
     </div>
 
 {{ Form::close() }}
