2c9bc173d976ec3e78fa041eacddc3280a51d548
Milena Petkanova <milena.petkanova@motion-software.com>
2020-03-08 23:16:00 +0200

    create partial for contact data

---
+++ resources/views/frontend/wishes/partial/wish-contact-data.blade.php
@@ -0,0 +1,64 @@
+@if ($logged_in_user['role'] == ('Seller') && count($wish->wishDetails->contacts) )
+    <h4>Kontaktdaten des Kunden</h4>
+    <div class="row">
+        <div class="col-md-3 c-info">
+            <i class="fal fa-pencil"></i>
+            <span>{{ $wish->wishDetails->contacts[0]->subject }}</span>
+        </div>
+        <div class="col-md-3 c-info">
+            <i class="fas fa-user"></i>
+            <span>{{ $wish->wishDetails->contacts[0]->name }}</span>
+        </div>
+        <div class="col-md-3 c-info c-tel">
+            <i class="fas fa-phone"></i>
+            <a href="tel:{{ $wish->wishDetails->contacts[0]->telephone }}">{{ $wish->wishDetails->contacts[0]->telephone }}</a>
+        </div>
+        <div class="col-md-3 c-info">
+            <i class="fas fa-envelope"></i>
+            <a href="mailto:{{ $wish->wishDetails->contacts[0]->email }}">{{ $wish->wishDetails->contacts[0]->email }}</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <p>
+                &nbsp;
+            </p>
+        </div>
+        <div class="col-md-12">
+            <h5>Nachricht:</h5>
+            <p>
+                {{ $wish->wishDetails->contacts[0]->message }}
+            </p>
+        </div>
+    </div>
+
+@else
+    <h4>{{ trans('wish.details.seller_title') }}</h4>
+    <div class="col-md-3 pl-0">
+        <p>
+            {{ $wish->wishDetails->group->users[0]->name }}
+        </p>
+        <p>
+            {{ $wish->wishDetails->group->users[0]->address }} <br>
+            {{ $wish->wishDetails->group->users[0]->zip_code }} {{ $wish->wishDetails->group->users[0]->city }}
+        </p>
+    </div>
+    @if(count($wish->wishDetails->offers) > 0 || (isset($wish->wishDetails->sellerMessages) && count($wish->wishDetails->sellerMessages) > 0 ))
+        <div class="col-md-3 c-info">
+            <i class="fas fa-user"></i>
+            <span>{{ $wish->wishDetails->group->users[0]->agents[0]->name }}</span>
+        </div>
+        <div class="col-md-3 c-info c-tel">
+            <i class="fas fa-phone"></i>
+            <a href="tel:{{ $wish->wishDetails->group->users[0]->agents[0]->telephone }}">{{ $wish->wishDetails->group->users[0]->agents[0]->telephone }}</a>            </div>
+        <div class="col-md-3 c-info">
+            <i class="fas fa-envelope"></i>
+            <a href="mailto:{{ $wish->wishDetails->group->users[0]->agents[0]->email }}">{{ $wish->wishDetails->group->users[0]->agents[0]->email }}</a>
+        </div>
+    @else
+        <div class="col-md-3 c-info">
+            <i class="fas fa-envelope"></i>
+            <a href="mailto:{{ $wish->wishDetails->group->users[0]->email }}">{{ $wish->wishDetails->group->users[0]->email }}</a>
+        </div>
+    @endif
+@endif
