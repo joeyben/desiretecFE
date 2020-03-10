96a7626fe575541e1711d9ef3b4980ad52379299
Pranav Pandhare <pranav.pandhare@desiretec.com>
2020-03-10 16:42:03 +0100

    agent and wishlist

--- app/Http/Requests/Agents/CreateAgentsRequest.php
+++ app/Http/Requests/Agents/CreateAgentsRequest.php
@@ -26,7 +26,7 @@ class CreateAgentsRequest extends FormRequest
         return [
             'name'  => 'required|string|max:64',
             'email' => 'required|email|max:255',
-            'telephone' => 'required|integer'
+            'telephone' => 'required'
         ];
     }
 
