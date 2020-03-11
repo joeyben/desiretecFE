3646f9e4d40477d5acb788a01cf30c779f93db36
Joe <joe@thisisleap.com>
2020-03-11 08:49:40 +0100

    new changes

--- resources/views/frontend/wishes/partial/wish-contact-data.blade.php
+++ resources/views/frontend/wishes/partial/wish-contact-data.blade.php
@@ -38,20 +38,24 @@
                 <div class="col-md-3 c-info">
                     <p>
                         {{ $wish->wishDetails->group->users[0]->name }}</p>
-                    <p>
-                        {{ $wish->wishDetails->group->users[0]->address }} <br>
-                        {{ $wish->wishDetails->group->users[0]->zip_code }} {{ $wish->wishDetails->group->users[0]->city }}
-                    </p>
+                    @if ($wish->wishDetails->group->users[0]->address)
+                        <p>
+                            {{ $wish->wishDetails->group->users[0]->address }} <br>
+                            {{ $wish->wishDetails->group->users[0]->zip_code }} {{ $wish->wishDetails->group->users[0]->city }}
+                        </p>
+                    @endif
                 </div>
                 @if(count($wish->wishDetails->offers) > 0 || count($wish->wishDetails->messages) > 0)
                     <div class="col-md-3 c-info">
                         <i class="fas fa-user"></i>
                         <span>{{ $wish->wishDetails->group->users[0]->agents[0]->name }}</span>
                     </div>
-                    <div class="col-md-3 c-info c-tel">
-                        <i class="fas fa-phone"></i>
-                        <a href="tel:{{ $wish->wishDetails->group->users[0]->agents[0]->telephone }}">{{ $wish->wishDetails->group->users[0]->agents[0]->telephone }}</a>
-                    </div>
+                    @if ($wish->wishDetails->group->users[0]->agents[0]->telephone)
+                        <div class="col-md-3 c-info c-tel">
+                            <i class="fas fa-phone"></i>
+                            <a href="tel:{{ $wish->wishDetails->group->users[0]->agents[0]->telephone }}">{{ $wish->wishDetails->group->users[0]->agents[0]->telephone }}</a>
+                        </div>
+                    @endif
                     <div class="col-md-3 c-info">
                         <i class="fas fa-envelope"></i>
                         <a href="mailto:{{ $wish->wishDetails->group->users[0]->agents[0]->email }}">{{ $wish->wishDetails->group->users[0]->agents[0]->email }}</a>
