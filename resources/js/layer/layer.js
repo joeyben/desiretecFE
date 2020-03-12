f4fbcaa6e348883cd7702638b61f57b5fcb15953
Joe <joe@thisisleap.com>
2020-03-12 12:12:48 +0100

    fix fonts

--- resources/js/layer/layer.js
+++ resources/js/layer/layer.js
@@ -14,7 +14,7 @@ var exitIntent = window.exitIntent || {};
     fontAwesomeIcons.attr({
         rel:  "stylesheet",
         type: "text/css",
-        href: "https://admin.desiretec.com/fontawsome/css/all.css"
+        href: "https://www.wish-service.com/fontawsome/css/all.css"
     });
     $("head").append(fontAwesomeIcons);
 
