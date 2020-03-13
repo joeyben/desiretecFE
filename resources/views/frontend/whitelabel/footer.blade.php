1ea9d2265d793dc4ed462686a10c9792e01a6189
Pranav Pandhare <pranav.pandhare@desiretec.com>
2020-03-13 16:21:33 +0100

    DD-574/footer position

--- resources/views/frontend/whitelabel/footer.blade.php
+++ resources/views/frontend/whitelabel/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="col-md-12">
             <ul class="d-flex justify-content-center flex-wrap">
-                @foreach (getWhitelabelInfo()['footers'] as $footer)
+                @foreach (getWhitelabelFooters() as $footer)
                      <li>
                         <a href="{{ $footer['url'] }}" target="_blank" class="link-btn-secondary">{{ $footer['name'] }}</a>
                     </li>
