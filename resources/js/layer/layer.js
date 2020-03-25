39b221d4aab4ff722841a81fdfc12003da7bd0c2
Milena Petkanova <milena.petkanova@motion-software.com>
2020-03-25 16:26:32 +0200

    rename variable

--- resources/js/layer/layer.js
+++ resources/js/layer/layer.js
@@ -2,9 +2,9 @@ var dt = window.dt || {};
 var exitIntent = window.exitIntent || {};
 
 if (!window.jQuery) {
-    var script = document.createElement('script');
-    script.src = "https://code.jquery.com/jquery-3.4.1.min.js";
-    document.getElementsByTagName('head')[0].appendChild(script);
+    var jQueryScript = document.createElement('script');
+    jQueryScript.src = "https://code.jquery.com/jquery-3.4.1.min.js";
+    document.getElementsByTagName('head')[0].appendChild(jQueryScript);
 }
 
 (function ($) {
