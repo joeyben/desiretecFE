7b6d1d0f296db1a16b2b352c29fb7a0470e01d2f
Joe <joe@thisisleap.com>
2020-06-25 12:29:31 +0200

    send session locale

--- app/Services/Api/ApiService.php
+++ app/Services/Api/ApiService.php
@@ -67,7 +67,6 @@ class ApiService implements ApiServiceInterface
         $this->setAuthorization(resolve('token'));
         $this->setHeader('c-agent', session()->get('c-agent', null));
         $this->setHeader('wl-id', session()->get('wl-id', null));
-
         $this->response = $this->client->get($this->apiUrl . $endpoint . '?' . http_build_query($data),
             [
                 'headers' => $this->headers
@@ -81,7 +80,6 @@ class ApiService implements ApiServiceInterface
     {
         $this->setAuthorization(resolve('token'));
         $this->setHeader('c-agent', session()->get('c-agent', null));
-        dd($this->headers);
         $this->response = $this->client->post($this->apiUrl . $endpoint,
             [
                 'headers' => $this->headers,
