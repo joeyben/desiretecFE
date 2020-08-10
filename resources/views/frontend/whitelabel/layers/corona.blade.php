8ebf99f999cca1b73ce370dd84b1eb30a345c6f1
Joe <joe@thisisleap.com>
2020-08-10 08:17:59 +0200

    fix tab-content class

--- resources/views/frontend/whitelabel/layers/corona.blade.php
+++ resources/views/frontend/whitelabel/layers/corona.blade.php
@@ -5,7 +5,7 @@
 
 @endphp
 
-<div id="{{ $layerName }}" class="tab-content">
+<div id="{{ $layerName }}" class="td-tab-content">
 
     <div class="kwp-header-dynamic">
         <div class="kwp-color-overlay"></div>
--- resources/views/frontend/whitelabel/layers/destination.blade.php
+++ resources/views/frontend/whitelabel/layers/destination.blade.php
@@ -2,7 +2,7 @@
     $layerName = 'destination';
 @endphp
 
-<div id="{{ $layerName }}" class="tab-content">
+<div id="{{ $layerName }}" class="td-tab-content">
 
     <div class="kwp-header-dynamic">
         <div class="kwp-color-overlay"></div>
